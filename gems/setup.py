2bf6d9000e56bd4705c8769d0019edd606377bd3
abhinavjain <abhinav@simpledatalabs.com>
2024-10-28 11:00:09 +0530

    prod tag 0.0.2

--- gems/setup.py
+++ gems/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 packages_to_include = find_packages(exclude = ['test.*', 'test', 'test_manual'])
 setup(
     name = 'prophecy_spark_alteryx',
-    version = '0.0.2.dev11',
+    version = '0.0.2',
     packages = packages_to_include,
     description = '',
     install_requires = [],
